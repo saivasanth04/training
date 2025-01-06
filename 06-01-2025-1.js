6a770487e80b430ff5d711856f44de4e8b8b9b5f
saivasanth04 <perurisaivasanth04@gmail.com>
2025-01-06 16:25:49 +0530

    message

---
+++ 06-01-2025-1.js
@@ -0,0 +1,21 @@
+console.log(10);
+let str1="double quotes";
+console.log(str1,typeof(str1));
+let str2='single quotes';
+console.log(str1,typeof(str2));
+let str3=`backticks quotes`;
+console.log(str1,typeof(str3));
+let bool1=true;
+console.log(bool1,typeof(bool1));
+let bool2=false;
+console.log(bool1,typeof(bool2));
+let num1=10;
+console.log(num1,typeof(num1))
+let num2=10.2345;
+console.log(num1,typeof(num2))
+let num3=10e3;
+console.log(num1,typeof(num3))
+let a;
+console.log(a,typeof(a));
+let x=null;
+console.log(x,typeof(x));
